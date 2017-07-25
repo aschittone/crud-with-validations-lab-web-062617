6341ba2cad4200702428d4efe8317d05305086fa
github username <github email address>
2017-07-25 17:26:56 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,11 @@
 Rails.application.routes.draw do
+  get "/songs", to: "songs#index"
+  get "/songs/new", to: "songs#new", as: "new_song"
+  post "/songs", to: "songs#create"
+  get "/songs/:id/edit", to: "songs#edit", as: "edit_song"
+  patch "/sings/:id", to: "songs#update", as: "song"
+  get "/songs/:id", to: "songs#show"
+  delete "/songs/:id", to: "songs#destroy"
+
+  root "songs#index"
 end
